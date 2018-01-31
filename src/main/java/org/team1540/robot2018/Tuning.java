3b44e282caa3f1681ac116c6d5884b435376e876
Nate S <nate.spy6@yahoo.com>
2018-01-30 17:03:51 -0800

    Added Intake Variables

--- src/main/java/org/team1540/robot2018/Tuning.java
+++ src/main/java/org/team1540/robot2018/Tuning.java
@@ -6,4 +6,18 @@ public class Tuning {
   @Tunable("Dead Zone")
   public static double deadZone = 0.1;
 
+  @Tunable("Auto Intake Spike Current")
+  public static double IntakeSpikeCurrent = 5.0;
+
+  @Tunable("Auto Intake Spike Length")
+  public static double IntakeSpikeLength = 1.0;
+
+  @Tunable("Auto Intake Speed")
+  public static double IntakeSpeed = 0.5;
+
+  @Tunable("Eject Seconds")
+  public static double EjectTime = 1.0;
+
+  @Tunable("Eject Speed")
+  public static double EjectSpeed = 0.5;
 }
