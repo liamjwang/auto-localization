b8c29882ad70d3effc1467d9405361a16022119f
Nate Sales <nate.spy6@yahoo.com>
2018-02-03 20:18:10 -0800

    Update motor numbers

--- src/main/java/org/team1540/robot2018/RobotMap.java
+++ src/main/java/org/team1540/robot2018/RobotMap.java
@@ -6,26 +6,26 @@ public class RobotMap {
   public static final int left2 = 2;
   public static final int left3 = 3;
 
-  public static final int right = 1;
-  public static final int right2 = 2;
-  public static final int right3 = 3;
+  public static final int right = 4;
+  public static final int right2 = 5;
+  public static final int right3 = 6;
 
-  public static final int elevator_1 = 8;
-  public static final int elevator_2 = 9;
+  public static final int wristMotor = 7;
 
-  public static final int intake_1 = 10;
-  public static final int intake_2 = 11;
+  public static final int tapeMeasureMotor = 8;
 
-  public static final int wristMotor = 12;
+  public static final int winchA = 9;
+  public static final int winchB = 10;
+  public static final int winchC = 11;
+  public static final int winchD = 12;
 
-  public static final int winchA = 13;
-  public static final int winchB = 14;
-  public static final int winchC = 15;
-  public static final int winchD = 16;
+  public static final int elevator_1 = 13;
+  public static final int elevator_2 = 14;
 
-  public static final int tapeMeasureMotor = 17;
+  public static final int intake_1 = 15;
+  public static final int intake_2 = 16;
 
-  public static final int panServo = 0;
-  public static final int tiltServo = 1;
+  public static final int panServo = 1;
+  public static final int tiltServo = 0;
 
 }
