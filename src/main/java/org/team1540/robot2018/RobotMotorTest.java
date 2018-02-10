7df73a8ba8605d2d690fb90e2d7b5a4853a9aec2
Nate Sales <nate.spy6@yahoo.com>
2018-02-09 19:26:00 -0800

    Add multiple motor controllers

--- src/main/java/org/team1540/robot2018/RobotMotorTest.java
+++ src/main/java/org/team1540/robot2018/RobotMotorTest.java
@@ -14,8 +14,10 @@ import org.team1540.base.wrappers.ChickenVictor;
 
 public class RobotMotorTest extends IterativeRobot {
 
-  private ChickenController[] motors = new ChickenController[17];
-  private SendableChooser<Integer> motorChooser;
+  private ChickenController[] motorsA = new ChickenController[17];
+  private ChickenController[] motorsB = new ChickenController[17];
+  private SendableChooser<Integer> motorChooserA;
+  private SendableChooser<Integer> motorChooserB;
 
   Boolean enableServo = false;
   double servoDivisor = 30;
@@ -26,37 +28,61 @@ public class RobotMotorTest extends IterativeRobot {
   @Override
   public void robotInit() {
     LiveWindow.disableAllTelemetry();
-    motorChooser = new SendableChooser<Integer>();
+    motorChooserA = new SendableChooser<Integer>();
+    motorChooserB = new SendableChooser<Integer>();
 
     for (int i = 1; i <= 16; i++) {
-      motorChooser.addObject(Integer.toString(i), i);
+      motorChooserA.addObject(Integer.toString(i), i);
+      motorChooserB.addObject(Integer.toString(i), i);
     }
 
-    SmartDashboard.putData("Motor Chooser", motorChooser);
+    SmartDashboard.putData("Motor ChooserA", motorChooserA);
+    SmartDashboard.putData("Motor ChooserB", motorChooserB);
     SmartDashboard.putData("PDP", new PowerDistributionPanel());
     SmartDashboard.putBoolean("Enable Servo Control", enableServo);
 
     SmartDashboard.putNumber("Servo Divisor", servoDivisor);
 
-    motors[1] = new ChickenTalon(1);
-    motors[2] = new ChickenTalon(2);
-    motors[3] = new ChickenTalon(3);
-    motors[4] = new ChickenTalon(4);
-    motors[5] = new ChickenTalon(5);
-    motors[6] = new ChickenTalon(6);
-    motors[7] = new ChickenTalon(7);
-    motors[8] = new ChickenVictor(8);
+    motorsA[1] = new ChickenTalon(1);
+    motorsA[2] = new ChickenTalon(2);
+    motorsA[3] = new ChickenTalon(3);
+    motorsA[4] = new ChickenTalon(4);
+    motorsA[5] = new ChickenTalon(5);
+    motorsA[6] = new ChickenTalon(6);
+    motorsA[7] = new ChickenTalon(7);
+    motorsA[8] = new ChickenVictor(8);
 
-    motors[9] = new ChickenTalon(9);
+    motorsA[9] = new ChickenTalon(9);
 
-    motors[10] = new ChickenVictor(10);
-    motors[11] = new ChickenVictor(11);
-    motors[12] = new ChickenVictor(12);
+    motorsA[10] = new ChickenVictor(10);
+    motorsA[11] = new ChickenVictor(11);
+    motorsA[12] = new ChickenVictor(12);
 
-    motors[13] = new ChickenTalon(13);
-    motors[14] = new ChickenTalon(14);
-    motors[15] = new ChickenVictor(15);
-    motors[16] = new ChickenVictor(16);
+    motorsA[13] = new ChickenTalon(13);
+    motorsA[14] = new ChickenTalon(14);
+    motorsA[15] = new ChickenVictor(15);
+    motorsA[16] = new ChickenVictor(16);
+
+
+    motorsB[1] = new ChickenTalon(1);
+    motorsB[2] = new ChickenTalon(2);
+    motorsB[3] = new ChickenTalon(3);
+    motorsB[4] = new ChickenTalon(4);
+    motorsB[5] = new ChickenTalon(5);
+    motorsB[6] = new ChickenTalon(6);
+    motorsB[7] = new ChickenTalon(7);
+    motorsB[8] = new ChickenVictor(8);
+
+    motorsB[9] = new ChickenTalon(9);
+
+    motorsB[10] = new ChickenVictor(10);
+    motorsB[11] = new ChickenVictor(11);
+    motorsB[12] = new ChickenVictor(12);
+
+    motorsB[13] = new ChickenTalon(13);
+    motorsB[14] = new ChickenTalon(14);
+    motorsB[15] = new ChickenVictor(15);
+    motorsB[16] = new ChickenVictor(16);
   }
 
   @Override
@@ -82,8 +108,11 @@ public class RobotMotorTest extends IterativeRobot {
     SmartDashboard.putNumber("Driver Left X", OI.getDriverLeftX());
 
     Scheduler.getInstance().run();
-    if (motorChooser.getSelected() != null) {
-      motors[motorChooser.getSelected()].set(ControlMode.PercentOutput, OI.getDriverRightY());
+    if (motorChooserA.getSelected() != null) {
+      motorsA[motorChooserA.getSelected()].set(ControlMode.PercentOutput, OI.getDriverRightY());
+    }
+    if (motorChooserB.getSelected() != null) {
+      motorsA[motorChooserB.getSelected()].set(ControlMode.PercentOutput, -OI.getDriverRightY());
     }
 
     if (SmartDashboard.getBoolean("Enable Servo Control", false)) {
