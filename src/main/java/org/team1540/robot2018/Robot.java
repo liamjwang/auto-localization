875e0708793e5b416870a6ee2e6266933ca481d4
Zachary Robinson <robinsonz@catlin.edu>
2018-02-11 11:03:34 -0800

    Optimize robot imports
    That setting should be on automatically, FYI

--- src/main/java/org/team1540/robot2018/Robot.java
+++ src/main/java/org/team1540/robot2018/Robot.java
@@ -1,28 +1,17 @@
 package org.team1540.robot2018;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.team1540.base.adjustables.AdjustableManager;
-
-import org.team1540.robot2018.commands.intake.AutoEject;
-import org.team1540.robot2018.commands.climber.WinchIn;
-import org.team1540.robot2018.commands.climber.WinchOut;
-import org.team1540.robot2018.subsystems.Climber;
-import org.team1540.robot2018.subsystems.DriveTrain;
-import org.team1540.robot2018.subsystems.Intake;
-import org.team1540.robot2018.subsystems.Elevator;
-import org.team1540.robot2018.subsystems.Wrist;
-
-import org.team1540.robot2018.commands.intake.AutoIntake;
+import org.team1540.robot2018.commands.elevator.ManualElevatorDown;
+import org.team1540.robot2018.commands.elevator.ManualElevatorUp;
 import org.team1540.robot2018.commands.intake.ManualEject;
 import org.team1540.robot2018.commands.intake.ManualIntake;
-import org.team1540.robot2018.commands.elevator.ManualElevatorUp;
-import org.team1540.robot2018.commands.elevator.ManualElevatorDown;
-
-import org.team1540.robot2018.commands.climber.TapeIn;
-import org.team1540.robot2018.commands.climber.TapeOut;
+import org.team1540.robot2018.subsystems.Climber;
+import org.team1540.robot2018.subsystems.DriveTrain;
+import org.team1540.robot2018.subsystems.Elevator;
+import org.team1540.robot2018.subsystems.Intake;
+import org.team1540.robot2018.subsystems.Wrist;
 
 public class Robot extends IterativeRobot {
   public static final DriveTrain drivetrain = new DriveTrain();
