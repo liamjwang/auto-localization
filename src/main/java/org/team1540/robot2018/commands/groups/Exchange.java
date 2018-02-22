ded1ed4e96db890f78c809cef060c98db1ce091a
Zachary Robinson <robinsonz@catlin.edu>
2018-02-21 20:52:32 -0800

    Remove MoveWristToPosition
    This also removes the elevator safety logic. Wrist commands will be
    added back in in a separate branch; however, they were not functional
    when Pandora went in the bag so they aren't going in master yet.

--- src/main/java/org/team1540/robot2018/commands/groups/Exchange.java
+++ src/main/java/org/team1540/robot2018/commands/groups/Exchange.java
@@ -3,12 +3,10 @@ package org.team1540.robot2018.commands.groups;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.team1540.robot2018.Tuning;
 import org.team1540.robot2018.commands.elevator.MoveElevator;
-import org.team1540.robot2018.commands.wrist.MoveWristToPosition;
 
 public class Exchange extends CommandGroup {
   public Exchange() {
-    addSequential(new MoveWristToPosition(Tuning.wristTransitPosition));
+    // TODO: Also move wrist all the way out
     addSequential(new MoveElevator(Tuning.elevatorExchangePosition));
-    addSequential(new MoveWristToPosition(Tuning.wristOutPosition));
   }
 }
--- src/main/java/org/team1540/robot2018/commands/groups/FrontSwitch.java
+++ src/main/java/org/team1540/robot2018/commands/groups/FrontSwitch.java
@@ -3,11 +3,10 @@ package org.team1540.robot2018.commands.groups;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.team1540.robot2018.Tuning;
 import org.team1540.robot2018.commands.elevator.MoveElevator;
-import org.team1540.robot2018.commands.wrist.MoveWristToPosition;
 
 public class FrontSwitch extends CommandGroup {
   public FrontSwitch() {
-    addSequential(new MoveWristToPosition(Tuning.wristOutPosition));
+    // TODO: Also move wrist all the way out
     addSequential(new MoveElevator(Tuning.elevatorFrontSwitchPosition));
   }
 }
