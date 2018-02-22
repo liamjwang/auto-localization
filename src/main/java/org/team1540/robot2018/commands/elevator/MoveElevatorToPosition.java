66a5264783d1e14f2a97114ad3b98e489f3bf586
Zachary Robinson <robinsonz@catlin.edu>
2018-02-21 22:33:39 -0800

    Add TODO to MoveElevatorToPosition

--- src/main/java/org/team1540/robot2018/commands/elevator/MoveElevatorToPosition.java
+++ src/main/java/org/team1540/robot2018/commands/elevator/MoveElevatorToPosition.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class MoveElevatorToPosition extends CommandGroup {
 
   public MoveElevatorToPosition(double target) {
+    // TODO: Add logic to prevent wrist colliding with supports
     addSequential(new MoveElevator(target));
   }
 }
