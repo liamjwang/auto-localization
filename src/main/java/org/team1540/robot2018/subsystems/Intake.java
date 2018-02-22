076abf3fdef197000c6148f66347bbcadadf680c
Zachary Robinson <robinsonz@catlin.edu>
2018-02-21 21:03:32 -0800

    Refactor intake
    Friends don't let friends use snake case.

--- src/main/java/org/team1540/robot2018/subsystems/Intake.java
+++ src/main/java/org/team1540/robot2018/subsystems/Intake.java
@@ -12,13 +12,13 @@ import org.team1540.robot2018.Tuning;
 public class Intake extends Subsystem {
   PowerDistributionPanel pdp = new PowerDistributionPanel();
 
-  private ChickenVictor intake_1 = new ChickenVictor(RobotMap.intake_1);
-  private ChickenVictor intake_2 = new ChickenVictor(RobotMap.intake_2);
+  private ChickenVictor intake1 = new ChickenVictor(RobotMap.intake_1);
+  private ChickenVictor intake2 = new ChickenVictor(RobotMap.intake_2);
   private double priority = 10;
 
   public Intake() {
-    intake_1.setInverted(true);
-    intake_2.setInverted(true);
+    intake1.setInverted(true);
+    intake2.setInverted(true);
   }
 
   @Override
@@ -27,7 +27,7 @@ public class Intake extends Subsystem {
 
   @Override
   public void periodic() {
-    SmartDashboard.putNumber("Current", getCurrent());
+    SmartDashboard.putNumber("Intake Current", getCurrent());
   }
 
   public double getCurrent() {
@@ -36,17 +36,17 @@ public class Intake extends Subsystem {
   }
 
   public void set(double value) {
-    intake_1.set(ControlMode.PercentOutput, value);
-    intake_2.set(ControlMode.PercentOutput, value);
+    intake1.set(ControlMode.PercentOutput, value);
+    intake2.set(ControlMode.PercentOutput, value);
   }
 
   public void set(double aValue, double bValue) {
-    intake_1.set(ControlMode.PercentOutput, aValue);
-    intake_2.set(ControlMode.PercentOutput, bValue);
+    intake1.set(ControlMode.PercentOutput, aValue);
+    intake2.set(ControlMode.PercentOutput, bValue);
   }
 
   public void stop() {
-    intake_1.set(ControlMode.PercentOutput, 0);
-    intake_2.set(ControlMode.PercentOutput, 0);
+    intake1.set(ControlMode.PercentOutput, 0);
+    intake2.set(ControlMode.PercentOutput, 0);
   }
 }
