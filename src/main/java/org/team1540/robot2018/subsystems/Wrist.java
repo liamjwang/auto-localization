595b95d8cdc3d4e3058f3857fd8c7e88ada592e3
Zachary Robinson <robinsonz@catlin.edu>
2018-02-24 19:05:45 -0800

    Remove HoldWristPosition stuff

--- src/main/java/org/team1540/robot2018/subsystems/Wrist.java
+++ src/main/java/org/team1540/robot2018/subsystems/Wrist.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import org.team1540.base.wrappers.ChickenTalon;
 import org.team1540.robot2018.RobotMap;
 import org.team1540.robot2018.Tuning;
-import org.team1540.robot2018.commands.wrist.HoldWristPosition;
+import org.team1540.robot2018.commands.wrist.JoystickWrist;
 
 public class Wrist extends Subsystem {
 
@@ -32,7 +32,7 @@ public class Wrist extends Subsystem {
 
   @Override
   public void initDefaultCommand() {
-    setDefaultCommand(new HoldWristPosition());
+    setDefaultCommand(new JoystickWrist());
   }
 
   public void set(double value) {
