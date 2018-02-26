22ea44c96b185c44c19832bf652e8d607d36ed7d
ambercmerrill <ambercmerrill@gmail.com>
2018-02-25 21:28:34 -0800

    Remove unused imports

--- src/main/java/org/team1540/robot2018/Robot.java
+++ src/main/java/org/team1540/robot2018/Robot.java
@@ -1,19 +1,11 @@
 package org.team1540.robot2018;
 
-import edu.wpi.cscore.CvSink;
-import edu.wpi.cscore.CvSource;
-import edu.wpi.cscore.UsbCamera;
-import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
 import org.team1540.base.adjustables.AdjustableManager;
 import org.team1540.base.util.SimpleCommand;
 import org.team1540.robot2018.commands.auto.StraightAuto;
