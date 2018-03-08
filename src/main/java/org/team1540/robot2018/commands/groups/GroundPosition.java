13976352c0569ad90edc04f4d8a9cf76e1745c4e
Zachary Robinson <robinsonz@catlin.edu>
2018-03-08 14:59:22 -0800

    Create and add CalibrateElevator to zero elevator

--- src/main/java/org/team1540/robot2018/commands/groups/GroundPosition.java
+++ src/main/java/org/team1540/robot2018/commands/groups/GroundPosition.java
@@ -2,16 +2,15 @@ package org.team1540.robot2018.commands.groups;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.team1540.robot2018.Tuning;
+import org.team1540.robot2018.commands.elevator.CalibrateElevator;
 import org.team1540.robot2018.commands.elevator.MoveElevatorToPosition;
 import org.team1540.robot2018.commands.wrist.CalibrateWrist;
-import org.team1540.robot2018.commands.wrist.MoveWristToPosition;
 
 public class GroundPosition extends CommandGroup {
   public GroundPosition() {
     // TODO: Also move wrist all the way out
-    // addSequential(new MoveWristToPosition(Tuning.wristOutPosition));
     addSequential(new CalibrateWrist());
-    // addSequential(new MoveElevatorToPosition(Tuning.elevatorGroundPosition));
     addSequential(new MoveElevatorToPosition(Tuning.elevatorGroundPosition));
+    addSequential(new CalibrateElevator());
   }
 }
