ce6db7f7e964b9db9db44fb3fedc29d5ccd703b1
Zachary Robinson <robinsonz@catlin.edu>
2018-03-09 11:46:48 -0800

    Revert "Break out profile generation"
    This reverts commit 15b6580
    
    Signed-off-by: Zachary Robinson <robinsonz@catlin.edu>

--- src/main/java/org/team1540/robot2018/Tuning.java
+++ src/main/java/org/team1540/robot2018/Tuning.java
@@ -256,10 +256,4 @@ public class Tuning {
   @Tunable("[Drivetrain] EncoderTPU")
   public static double drivetrainEncoderTPU;
 
-  @Tunable("Motion Profiling In Path")
-  public static String motionProfileInPath = "";
-
-  @Tunable("Motion Profiling Out Path")
-  public static String motionProfileOutPath = "";
-
 }
--- src/main/java/org/team1540/robot2018/commands/auto/AutonomousProfiling.java
+++ src/main/java/org/team1540/robot2018/commands/auto/AutonomousProfiling.java
@@ -1,5 +1,7 @@
 package org.team1540.robot2018.commands.auto;
 
+import static org.team1540.robot2018.Tuning.timeStep;
+
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
@@ -10,21 +12,40 @@ import jaci.pathfinder.Pathfinder;
 import jaci.pathfinder.Trajectory;
 import jaci.pathfinder.Trajectory.Config;
 import jaci.pathfinder.Trajectory.FitMethod;
+import jaci.pathfinder.Trajectory.Segment;
 import jaci.pathfinder.Waypoint;
+import jaci.pathfinder.modifiers.TankModifier;
+import java.util.LinkedList;
+import java.util.List;
 import openrio.powerup.MatchData;
 import openrio.powerup.MatchData.GameFeature;
 import openrio.powerup.MatchData.OwnedSide;
 import org.team1540.base.motionprofiling.MotionProfilingProperties;
-import org.team1540.base.motionprofiling.RunMotionProfiles;
 import org.team1540.robot2018.Robot;
 import org.team1540.robot2018.Tuning;
-import org.team1540.robot2018.commands.auto.ProfileIO.MotionProfilingTypes;
-import org.team1540.robot2018.commands.auto.ProfileIO.StartLocation;
 
 public class AutonomousProfiling extends Command {
 
   // TODO Any changes based on the fact that the center of turning isn't in the center?
 
+  private static final double BOT_BUMPER_WIDTH = 3.25;
+  private static final double BOT_LENGTH = 32.625;
+  private static final double BOT_EFFECTIVE_LENGTH = BOT_LENGTH + 2 * BOT_BUMPER_WIDTH;
+  private static final double BOT_SHORT_DISTANCE_TO_TURNING = 27.75;
+  private static final double BOT_WIDTH = 27.75;
+  private static final double BOT_EFFECTIVE_WIDTH = BOT_WIDTH + 2 * BOT_BUMPER_WIDTH;
+
+  // X is the long side of the field, Y is the short side.
+  // All measurements are relative to your alliance wall, since the field is fully symmetrical.
+  private static final double FIELD_EFFECTIVE_LENGTH = 647.125;
+  private static final double FIELD_EFFECTIVE_WIDTH = 324.5;
+  // TODO Better way of storing these measurements
+  private static final double FIELD_EXCHANGE_NEAR_DISTANCE_FROM_CENTER = 12;
+  private static final double FIELD_PORTAL_HEIGHT = 60;
+  private static final double FIELD_STARTING_LINE = 30;
+  private static final double FIELD_SWITCH_MIDDLE_TO_CENTER = 54.53;
+  private static final double FIELD_SWITCH_TO_ALLIANCE_WALL = 151;
+  private static final double FIELD_SWITCH_TO_SIDE_WALLS = 85.75;
   private SendableChooser<AutoType> autoTypeChooser = new SendableChooser<>();
   private Trajectory.FitMethod fitMethod = FitMethod.HERMITE_CUBIC;
   private Timer isFinishedRunningTimer = new Timer();
@@ -40,24 +61,27 @@ public class AutonomousProfiling extends Command {
   private TrajectorySegment[] segments;
   private SendableChooser<StartLocation> startLocationChooser = new SendableChooser<>();
   private double timeToFinish = 0;
+  // TODO Store these profiles instead of generating on the fly
+  private List<Waypoint> waypoints = new LinkedList<>();
 
-  public AutonomousProfiling() {
-    // TODO Can this be done at declaration?
-    // TODO Is adding stuff to SmartDashboard in a decentralized manner okay?
-    // FIXME null pointers for days
-    for (AutoType type : AutoType.values()) {
-      autoTypeChooser.addObject(type.name(), type);
-    }
-    autoTypeChooser.addDefault("DEFAULT: SWITCH_ALL_SIDES", AutoType.SWITCH_ALL_SIDES);
-    autoTypeChooser.setName("Auto Type");
-    SmartDashboard.putData(autoTypeChooser);
-    for (StartLocation type : StartLocation.values()) {
-      startLocationChooser.addObject(type.name(), type);
-    }
-    startLocationChooser.addDefault("DEFAULT: RIGHT_EDGE", StartLocation.RIGHT_EDGE);
-    startLocationChooser.setName("Start Location");
-    SmartDashboard.putData(startLocationChooser);
-  }
+  // public AutonomousProfiling() {
+  //   // TODO Can this be done at declaration?
+  //   // TODO Is adding stuff to SmartDashboard in a decentralized manner okay?
+  //   // FIXME null pointers for days
+  //   for (AutoType type : AutoType.values()) {
+  //     autoTypeChooser.addObject(type.name(), type);
+  //   }
+  //   autoTypeChooser.addDefault("DEFAULT: SWITCH_ALL_SIDES", AutoType.SWITCH_ALL_SIDES);
+  //   autoTypeChooser.setName("Auto Type");
+  //   SmartDashboard.putData(autoTypeChooser);
+  //   for (StartLocation type : StartLocation.values()) {
+  //     startLocationChooser.addObject(type.name(), type);
+  //   }
+  //   startLocationChooser.addDefault("DEFAULT: RIGHT_EDGE", StartLocation.RIGHT_EDGE);
+  //   startLocationChooser.setName("Start Location");
+  //   SmartDashboard.putData(startLocationChooser);
+  //   // AdjustableManager.getInstance().add(this);
+  // }
 
   public AutonomousProfiling(TrajectorySegment... segments) {
     requires(Robot.drivetrain);
@@ -65,44 +89,49 @@ public class AutonomousProfiling extends Command {
     System.out.println(segments[0].start.y + " ---------------- " + segments[0].end.y);
   }
 
-  // private Trajectory generateTestTrajectory(Trajectory.Config config) {
-  //   return Pathfinder.generate(new Waypoint[]{
-  //       new Waypoint(0, 0, 0), new Waypoint
-  //       (Tuning.distanceToTravelX, Tuning.distanceToTravelY,
-  //           Pathfinder.d2r(Tuning.degreesToTurn))
-  //   }, config);
-  // }
+  private Trajectory generateTestTrajectory(Trajectory.Config config) {
+    return Pathfinder.generate(new Waypoint[]{
+        new Waypoint(0, 0, 0), new Waypoint
+        (Tuning.distanceToTravelX, Tuning.distanceToTravelY,
+            Pathfinder.d2r(Tuning.degreesToTurn))
+    }, config);
+  }
 
-  @Override
-  protected void initialize() {
-    Robot.drivetrain.zeroEncoders();
+  public Trajectory generateTrajectory(Trajectory.Config config, AutoType autoType, StartLocation
+      startLocation) {
+    waypoints.clear();
 
-    // FIXME yeah this is totally good
-    Trajectory[] trajectories = new Trajectory[]{null};
+    double xStraight = 130;
+    double xSide = 100;
+    double ySide = 90;
+    double degrees = Pathfinder.d2r(-15);
 
-    switch (autoTypeChooser.getSelected()) {
+    // FIXME Temp hard coded values, doesn't even listen to start position
+    switch (autoType) {
       case CROSS_LINE:
-        trajectories = ProfileIO.loadProfileFromCSV(MotionProfilingTypes.CROSS_LINE);
+        waypoints.add(new Waypoint(0, 0, 0));
+        waypoints.add(new Waypoint(130, 0, 0));
         break;
       case SWITCH_SAME_SIDE:
+        waypoints.add(new Waypoint(0, 0, 0));
         if (MatchData.getOwnedSide(GameFeature.SWITCH_NEAR) == OwnedSide.LEFT
-            && startLocationChooser.getSelected() == StartLocation.LEFT_EDGE) {
-          trajectories = ProfileIO.loadProfileFromCSV(MotionProfilingTypes.CENTER_TO_SWITCH_LEFT);
+            && startLocation == StartLocation.LEFT_EDGE) {
+          waypoints.add(new Waypoint(xSide, -ySide, -degrees));
         } else if (MatchData.getOwnedSide(GameFeature.SWITCH_NEAR) == OwnedSide.RIGHT
-            && startLocationChooser.getSelected() == StartLocation.RIGHT_EDGE) {
-          trajectories = ProfileIO.loadProfileFromCSV(MotionProfilingTypes.CENTER_TO_SWITCH_RIGHT);
+            && startLocation == StartLocation.RIGHT_EDGE) {
+          waypoints.add(new Waypoint(xSide, ySide, degrees));
         } else {
-          trajectories = ProfileIO.loadProfileFromCSV(MotionProfilingTypes.CROSS_LINE);
+          // Just cross the line
+          waypoints.add(new Waypoint(130, 0, 0));
         }
         break;
       case SWITCH_ALL_SIDES:
         switch (MatchData.getOwnedSide(GameFeature.SWITCH_NEAR)) {
           case LEFT:
-            trajectories = ProfileIO.loadProfileFromCSV(MotionProfilingTypes.CENTER_TO_SWITCH_LEFT);
+            waypoints.add(new Waypoint(xSide, -ySide, -degrees));
             break;
           case RIGHT:
-            trajectories = ProfileIO.loadProfileFromCSV(MotionProfilingTypes
-                .CENTER_TO_SWITCH_RIGHT);
+            waypoints.add(new Waypoint(xSide, ySide, degrees));
             break;
         }
         break;
@@ -110,7 +139,83 @@ public class AutonomousProfiling extends Command {
         break;
     }
 
-    timeToFinish = trajectories[0].segments.length * trajectories[0].segments[0].dt;
+    /*
+    waypoints.add(startLocation.getLocation());
+    switch (autoType) {
+      case CROSS_LINE:
+        switch (startLocation) {
+          case EXCHANGE_MIDDLE:
+            throw new InvalidPathException(
+                "Auto type " + autoType + " is not valid for starting position " + startLocation);
+          default:
+            waypoints.add(makeTranslatedWaypoint(waypoints.get(0),
+                FIELD_STARTING_LINE + BOT_EFFECTIVE_LENGTH, 0, 0));
+            break;
+        }
+        break;
+      case SWITCH_SAME_SIDE:
+        if (MatchData.getOwnedSide(GameFeature.SWITCH_NEAR) == OwnedSide.LEFT
+            && startLocation == StartLocation.LEFT_EDGE) {
+          waypoints.add(EndLocation.LEFT_SWITCH_SIDE.getLocation());
+        } else if (MatchData.getOwnedSide(GameFeature.SWITCH_NEAR) == OwnedSide.RIGHT
+            && startLocation == StartLocation.RIGHT_EDGE) {
+          waypoints.add(EndLocation.RIGHT_SWITCH_SIDE.getLocation());
+        } else {
+          // Just cross the line
+          waypoints.add(makeTranslatedWaypoint(waypoints.get(0),
+              FIELD_STARTING_LINE + BOT_EFFECTIVE_LENGTH, 0, 0));
+        }
+        break;
+      case SWITCH_ALL_SIDES:
+        switch (MatchData.getOwnedSide(GameFeature.SWITCH_NEAR)) {
+          case LEFT:
+            waypoints.add(EndLocation.LEFT_SWITCH_SIDE.getLocation());
+            break;
+          case RIGHT:
+            waypoints.add(EndLocation.RIGHT_SWITCH_SIDE.getLocation());
+            break;
+        }
+        break;
+      case SWITCH_TOP:
+        // TODO
+        break;
+    }
+    */
+
+    return Pathfinder.generate(waypoints.toArray(new Waypoint[0]), config);
+  }
+
+  @Override
+  protected void initialize() {
+    Robot.drivetrain.zeroEncoders();
+    // FIXME YOU DO NOT PASS IN THE TURNING RADIUS YOU PASS IN THE WHEELBASE WIDTH
+    double turningRadius = Math.sqrt(Math.pow(Tuning.wheelbaseWidth, 2) + Math.pow
+        (Tuning.distanceBetweenWheels, 2));
+
+    // // TODO Yes this is very safe honhonhon
+    // Trajectory trajectory;
+    // try {
+    //   trajectory = generateTrajectory(new Config(fitMethod, Tuning.sampleRate, timeStep,
+    // Tuning.maxVelocity,
+    //           Tuning.maxAcceleration, Tuning.maxJerk),
+    //       autoTypeChooser.getSelected(), startLocationChooser.getSelected());
+    //   // TODO exception handling
+    // } catch (InvalidPathException e) {
+    //   e.printStackTrace();
+    //   return;
+    // }
+
+
+    Config config = new Config(fitMethod, Tuning.sampleRate, timeStep,
+        Tuning.maxVelocity, Tuning.maxAcceleration, Tuning.maxJerk);
+
+    List<Segment> leftSegments = new LinkedList<>();
+    Trajectory trajectory = generateSimpleTrajectory(segments[0].start, segments[0].end, config);
+    TankModifier modifier = new TankModifier(trajectory).modify(turningRadius);
+    Segment[] left = modifier.getLeftTrajectory().segments;
+    Segment[] right = modifier.getRightTrajectory().segments;
+
+    timeToFinish = left.length * left[0].dt;
     // for (TrajectorySegment segment : segments) {
     //   Trajectory traj = generateSimpleTrajectory(segment.start, segment.end, config);
     //   timeToFinish += traj.segments.length * traj.segments[0].dt;
@@ -138,16 +243,14 @@ public class AutonomousProfiling extends Command {
         (Tuning.lEncoderTicksPerUnit, Tuning.secondsFromNeutralToFull, Robot
             .drivetrain::getLeftVelocity,
             Robot.drivetrain::setLeftVelocity, Robot.drivetrain::getLeftPosition,
-            trajectories[0]);
+            modifier.getLeftTrajectory());
     MotionProfilingProperties rightProperties = new MotionProfilingProperties
         (Tuning.rEncoderTicksPerUnit, Tuning.secondsFromNeutralToFull, Robot
             .drivetrain::getRightVelocity,
             Robot.drivetrain::setRightVelocity, Robot.drivetrain::getRightPosition,
-            trajectories[1]);
-
+            modifier.getRightTrajectory());
     isFinishedRunningTimer.reset();
-    runMotionProfiles = new RunMotionProfiles(leftProperties, rightProperties);
-    Scheduler.getInstance().add(runMotionProfiles);
+    Scheduler.getInstance().add(new RunMotionProfiles(leftProperties, rightProperties));
     isFinishedRunningTimer.start();
   }
 
@@ -172,17 +275,52 @@ public class AutonomousProfiling extends Command {
     return isFinishedRunningTimer.get() > timeToFinish;
   }
 
-  @Override
-  protected void interrupted() {
-    runMotionProfiles.cancel();
-  }
-
   public enum AutoType {
     CROSS_LINE, SWITCH_SAME_SIDE, SWITCH_ALL_SIDES, SWITCH_TOP
   }
 
   public interface Location {
     public Waypoint getLocation();
+  }
+
+  public enum StartLocation implements Location {
+    LEFT_EDGE(new Waypoint(
+        BOT_EFFECTIVE_LENGTH / 2, FIELD_EFFECTIVE_WIDTH - FIELD_PORTAL_HEIGHT, 0)),
+    EXCHANGE_MIDDLE(new Waypoint(
+        BOT_EFFECTIVE_LENGTH / 2,
+        FIELD_EFFECTIVE_WIDTH / 2 + FIELD_EXCHANGE_NEAR_DISTANCE_FROM_CENTER
+            - BOT_EFFECTIVE_WIDTH / 2, 0)),
+    RIGHT_EDGE(new Waypoint(BOT_EFFECTIVE_LENGTH / 2, FIELD_PORTAL_HEIGHT, 0));
+
+    private final Waypoint location;
+
+    StartLocation(Waypoint location) {
+      this.location = location;
+    }
+
+    @Override
+    public Waypoint getLocation() {
+      return location;
+    }
+  }
+
+  public enum EndLocation implements Location {
+    LEFT_SWITCH_SIDE(new Waypoint(
+        FIELD_SWITCH_TO_ALLIANCE_WALL - BOT_EFFECTIVE_LENGTH / 2,
+        FIELD_EFFECTIVE_WIDTH - FIELD_SWITCH_MIDDLE_TO_CENTER, 0)),
+    RIGHT_SWITCH_SIDE(new Waypoint(FIELD_SWITCH_TO_ALLIANCE_WALL
+        - BOT_EFFECTIVE_LENGTH / 2, FIELD_SWITCH_MIDDLE_TO_CENTER, 0));
+
+    private final Waypoint location;
+
+    EndLocation(Waypoint location) {
+      this.location = location;
+    }
+
+    @Override
+    public Waypoint getLocation() {
+      return location;
+    }
   }
 
   public static class TrajectorySegment {
