9437766675842122324bfc89ca1902be04438ed5
wangl5 <wangl5@users.noreply.github.com>
2018-03-09 06:32:10 -0800

    Make ground position command zero the elevator

--- src/main/java/org/team1540/robot2018/commands/groups/GroundPosition.java
+++ src/main/java/org/team1540/robot2018/commands/groups/GroundPosition.java
@@ -1,8 +1,12 @@
 package org.team1540.robot2018.commands.groups;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.ConditionalCommand;
+import edu.wpi.first.wpilibj.command.InstantCommand;
+import org.team1540.robot2018.Robot;
 import org.team1540.robot2018.Tuning;
 import org.team1540.robot2018.commands.elevator.CalibrateElevator;
+import org.team1540.robot2018.commands.elevator.MoveElevator;
 import org.team1540.robot2018.commands.elevator.MoveElevatorToPosition;
 import org.team1540.robot2018.commands.wrist.CalibrateWrist;
 
@@ -10,7 +14,13 @@ public class GroundPosition extends CommandGroup {
   public GroundPosition() {
     // TODO: Also move wrist all the way out
     addSequential(new CalibrateWrist());
-    addSequential(new MoveElevatorToPosition(Tuning.elevatorGroundPosition));
+    addSequential(new ConditionalCommand(new MoveElevator(Tuning.elevatorGroundPosition)) {
+      @Override
+      protected boolean condition() {
+        return Robot.elevator.getPosition() > Tuning.elevatorGroundPosition;
+      }
+    });
     addSequential(new CalibrateElevator());
+    addSequential(new InstantCommand());
   }
 }
