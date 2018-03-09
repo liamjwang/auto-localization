d8295a588db1098956ea7af65bf7141298fdc11d
Zachary Robinson <robinsonz@catlin.edu>
2018-03-09 14:44:42 -0800

    Remove wrist from intake sequence
    Signed-off-by: Zachary Robinson <robinsonz@catlin.edu>

--- src/main/java/org/team1540/robot2018/commands/groups/IntakeSequence.java
+++ src/main/java/org/team1540/robot2018/commands/groups/IntakeSequence.java
@@ -1,9 +1,7 @@
 package org.team1540.robot2018.commands.groups;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import org.team1540.robot2018.Tuning;
 import org.team1540.robot2018.commands.intake.AutoIntake;
-import org.team1540.robot2018.commands.wrist.MoveWristToPosition;
 
 /**
  * Lower wrist > lower intake > intake a cube > move wrist up
@@ -12,6 +10,6 @@ public class IntakeSequence extends CommandGroup {
   public IntakeSequence() {
     // addSequential(new GroundPosition());
     addSequential(new AutoIntake());
-    addSequential(new MoveWristToPosition(Tuning.wrist45FwdPosition));
+    // addSequential(new MoveWristToPosition(Tuning.wrist45FwdPosition));
   }
 }
