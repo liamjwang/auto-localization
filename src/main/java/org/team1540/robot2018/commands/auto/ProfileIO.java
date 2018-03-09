4349528c3de4a21bd44e475ca6c8b1322b37e24f
Zachary Robinson <robinsonz@catlin.edu>
2018-03-09 11:46:36 -0800

    Revert "Clean up code, comments"
    This reverts commit 5fd78fe
    
    Signed-off-by: Zachary Robinson <robinsonz@catlin.edu>

--- src/main/java/org/team1540/robot2018/commands/auto/ProfileIO.java
+++ src/main/java/org/team1540/robot2018/commands/auto/ProfileIO.java
@@ -79,8 +79,6 @@ public class ProfileIO {
   static double degrees = Pathfinder.d2r(-15);
   static Trajectory.FitMethod fitMethod = FitMethod.HERMITE_CUBIC;
 
-  // To execute this locally, you'll need to set up Pathfinder using a local version of the lib
-  // instead of the Gradle version for reasons. Have fun!
   public static void main(String[] args) {
     writeAllTrajectories();
   }
@@ -94,7 +92,7 @@ public class ProfileIO {
   public static void writeProfileToCSV(MotionProfilingTypes toWrite) {
     TankModifier modifier = new TankModifier(Pathfinder.generate(toWrite.waypoints, new Config
         (fitMethod, Tuning.sampleRate, timeStep,
-            Tuning.maxVelocity, Tuning.maxAcceleration, Tuning.maxJerk))).modify(Math.sqrt(
+        Tuning.maxVelocity, Tuning.maxAcceleration, Tuning.maxJerk))).modify(Math.sqrt(
         Math.pow(Tuning.wheelbaseWidth, 2) + Math.pow
             (Tuning.distanceBetweenWheels, 2)));
     Pathfinder.writeToCSV(new File(
@@ -114,14 +112,9 @@ public class ProfileIO {
   public enum MotionProfilingTypes {
 
     CROSS_LINE(new Waypoint[]{new Waypoint(0, 0, 0), new Waypoint(130, 0, 0)}),
-    CENTER_TO_SWITCH_LEFT(new Waypoint[]{
-        new Waypoint(0, 0, 0), new Waypoint(xSide, -ySide,
-        -degrees)
-    }),
-    CENTER_TO_SWITCH_RIGHT(new Waypoint[]{
-        new Waypoint(0, 0, 0), new Waypoint(xSide, ySide,
-        degrees)
-    });
+    CENTER_TO_SWITCH_LEFT(new Waypoint[]{new Waypoint(0, 0, 0), new Waypoint(xSide, -ySide,
+        -degrees)}),
+    CENTER_TO_SWITCH_RIGHT(new Waypoint[]{new Waypoint(0, 0, 0), new Waypoint(xSide, ySide, degrees)});
 
     public final Waypoint[] waypoints;
 
