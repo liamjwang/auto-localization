a19288ee311fca791e865ec27fbaf2aa718bded8
wangl5 <wangl5@users.noreply.github.com>
2018-03-11 22:14:46 -0700

    Remove outdated comments

--- src/main/java/org/team1540/robot2018/commands/groups/GroundPosition.java
+++ src/main/java/org/team1540/robot2018/commands/groups/GroundPosition.java
@@ -7,12 +7,10 @@ import org.team1540.robot2018.Robot;
 import org.team1540.robot2018.Tuning;
 import org.team1540.robot2018.commands.elevator.CalibrateElevator;
 import org.team1540.robot2018.commands.elevator.MoveElevator;
-import org.team1540.robot2018.commands.elevator.MoveElevatorToPosition;
 import org.team1540.robot2018.commands.wrist.CalibrateWrist;
 
 public class GroundPosition extends CommandGroup {
   public GroundPosition() {
-    // TODO: Also move wrist all the way out
     addSequential(new CalibrateWrist());
     addSequential(new ConditionalCommand(new MoveElevator(Tuning.elevatorGroundPosition)) {
       @Override
--- src/main/java/org/team1540/robot2018/commands/groups/IntakeSequence.java
+++ src/main/java/org/team1540/robot2018/commands/groups/IntakeSequence.java
@@ -3,9 +3,6 @@ package org.team1540.robot2018.commands.groups;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.team1540.robot2018.commands.intake.AutoIntake;
 
-/**
- * Lower wrist > lower intake > intake a cube > move wrist up
- */
 public class IntakeSequence extends CommandGroup {
   public IntakeSequence() {
     // addSequential(new GroundPosition());
