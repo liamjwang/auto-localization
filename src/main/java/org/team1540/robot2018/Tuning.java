3454746e5c0dc4dd771592661e1737f50c86206a
wangl5 <wangl5@users.noreply.github.com>
2018-03-13 12:14:45 -0700

    Revert "Remove tuning values from SmartDashboard that we never tune with SmartDashboard"
    This reverts commit 1fa93a6

--- src/main/java/org/team1540/robot2018/Tuning.java
+++ src/main/java/org/team1540/robot2018/Tuning.java
@@ -25,25 +25,35 @@ public class Tuning {
   public static double stupidDriveTime = 2.5;
 
   // INTAKE
+  @Tunable("[Intake] Auto Intake Spike Current")
   public static double intakeSpikeCurrent = 30.0;
 
+  @Tunable("[Intake] Auto Intake Speed Motor A")
   public static double intakeSpeedA = -1;
 
+  @Tunable("[Intake] Auto Intake Speed Motor B")
   public static double intakeSpeedB = -0.4;
 
+  @Tunable("[Intake] Auto Intake Min Time")
   public static double intakeMinTime = 1;
 
+  @Tunable("[Intake] Auto Intake Max Time")
   public static double intakeMaxTime = 10;
 
+  @Tunable("[Intake] Intake Hold Speed")
   public static double intakeHoldSpeed = -0.1;
 
+  @Tunable("[Intake] Eject Seconds")
   public static double intakeEjectTime = 1.0;
 
+  @Tunable("[Intake] Eject Speed Motor A")
   public static double intakeEjectSpeed = 0.8;
 
   // ARMS
+  @Tunable("[Intake] Arm Hold Speed")
   public static double armHoldSpeed = -0.1;
 
+  @Tunable("[Intake] Arm Joystick Constant")
   public static double armJoystickConstant = 0.5;
 
   // ELEVATOR
@@ -53,12 +63,15 @@ public class Tuning {
 
   public static double elevatorD = 10;
 
+  @Tunable("[Elevator] kF Going Up")
   public static double elevatorFGoingUp = 2.046;
 
+  @Tunable("[Elevator] kF Going Down")
   public static double elevatorFGoingDown = 1.2;
 
   public static int elevatorIZone = 150;
 
+  @Tunable("[Elevator] Error Tolerance")
   public static double elevatorTolerance = 100;
 
   public static int elevatorMaxAccel = 750;
@@ -77,14 +90,19 @@ public class Tuning {
 
   public static double elevatorObstaclePosition = 1300;
 
+  @Tunable("[Elevator] Obstacle Upper Position")
   public static double elevatorObstacleUpperPosition = 3750;
 
+  @Tunable("[Elevator] Max Elevator Deviation")
   public static double elevatorMaxDeviation = 200;
 
+  @Tunable("[Elevator] Down Multiplier")
   public static double elevatorDownMult = 0.5;
 
+  @Tunable("[Elevator] Max Spike Time")
   public static double elevatorSpikeTime = 0;
 
+  @Tunable("[Elevator] Max Spike Current")
   public static double elevatorCurrentThreshold = 60;
 
   // WRIST
@@ -102,12 +120,16 @@ public class Tuning {
 
   public static int wristMaxAccel = 1000;
 
+  @Tunable("[Wrist] Peak Current Limit")
   public static int wristCurrentLimit = 30;
 
+  @Tunable("[Wrist] Peak Duration")
   public static int wristPeakDuration = 0;
 
+  @Tunable("[Wrist] Stall Current")
   public static double wristStallCurrent = 30;
 
+  @Tunable("[Wrist] Stop Tolerance")
   public static double wristTolerance = 50;
 
   public static double wristOutPosition = 8250;
@@ -120,6 +142,7 @@ public class Tuning {
 
   public static double wrist45FwdPosition = 6200;
 
+  @Tunable("[Wrist] Max Wrist Deviation")
   public static double maxWristDeviation = 200;
 
   // DRIVETRAIN
@@ -145,26 +168,36 @@ public class Tuning {
 
   public static double drivetrainEncoderTPU;
 
+  @Tunable("[MotionP] MaxVelocity")
   public static double profileMaxVel = 40;
 
+  @Tunable("[MotionP] MaxAcceleration")
   public static double profileMaxAccel = 40;
 
+  @Tunable("[MotionP] MaxJerk")
   public static double profileMaxJerk = 2300;
 
+  @Tunable("[MotionP] SecondsFromNeutralToFull")
   public static double profileSecondsFromNeutralToFull = 0;
 
+  @Tunable("[MotionP] SampleRate")
   public static int profileSampleRate = Config.SAMPLES_FAST;
 
+  @Tunable("[MotionP] TimeStep")
   public static double profileTimeStep = 0.05;
 
   public static FitMethod profileFitMethod = FitMethod.HERMITE_CUBIC;
 
+  @Tunable("[MotionP] LeftEncoderTicksPerUnit")
   public static double profileLeftTPU = 52;
 
+  @Tunable("[MotionP] RightEncoderTicksPerUnit")
   public static double profileRightTPU = 52;
 
+  @Tunable("[MotionP] WheelbaseWidth")
   public static double profileBaseWidth = 25.091;
 
   // TODO: Rename profileWheelDistance to be more specific
+  @Tunable("[MotionP] distanceBetweenWheels")
   public static double profileWheelDistance = 11.812;
 }
