1fa93a659ec4bac257cde5d063499518b1d594bb
wangl5 <wangl5@users.noreply.github.com>
2018-03-13 11:51:58 -0700

    Remove tuning values from SmartDashboard that we never tune with SmartDashboard

--- src/main/java/org/team1540/robot2018/Tuning.java
+++ src/main/java/org/team1540/robot2018/Tuning.java
@@ -25,37 +25,26 @@ public class Tuning {
   public static double stupidDriveTime = 2.5;
 
   // INTAKE
-  @Tunable("[Intake] Auto Intake Spike Current")
   public static double intakeSpikeCurrent = 30.0;
 
-  @Tunable("[Intake] Auto Intake Speed Motor A")
   public static double intakeSpeedA = -1;
 
-  @Tunable("[Intake] Auto Intake Speed Motor B")
   public static double intakeSpeedB = -0.4;
 
-  @Tunable("[Intake] Auto Intake Min Time")
   public static double intakeMinTime = 1;
 
-  @Tunable("[Intake] Auto Intake Max Time")
   public static double intakeMaxTime = 10;
 
-  @Tunable("[Intake] Intake Hold Speed")
   public static double intakeHoldSpeed = -0.1;
 
-  @Tunable("[Intake] Eject Seconds")
   public static double ejectTime = 1.0;
 
-  @Tunable("[Intake] Eject Speed Motor A")
   public static double ejectSpeedA = 0.8;
 
-  @Tunable("[Intake] Eject Speed Motor B")
   public static double ejectSpeedB = 0.8;
 
-  @Tunable("[Intake] Arm Hold Speed")
   public static double intakeArmHoldSpeed = -0.1;
 
-  @Tunable("[Intake] Arm Joystick Constant")
   public static double intakeArmJoystickConstant = 0.5;
 
   // ELEVATOR
@@ -65,15 +54,12 @@ public class Tuning {
 
   public static double elevatorD = 10;
 
-  @Tunable("[Elevator] kF Going Up")
   public static double elevatorFGoingUp = 2.046;
 
-  @Tunable("[Elevator] kF Going Down")
   public static double elevatorFGoingDown = 1.2;
 
   public static int elevatorIZone = 150;
 
-  @Tunable("[Elevator] Error Tolerance")
   public static double elevatorTolerance = 100;
 
   public static int elevatorMaxAccel = 750;
@@ -92,22 +78,16 @@ public class Tuning {
 
   public static double elevatorRungPosition; //TODO
 
-  @Tunable("[Elevator] Obstacle Position")
   public static double elevatorObstaclePosition = 1300;
 
-  @Tunable("[Elevator] Obstacle Upper Position")
   public static double elevatorObstacleUpperPosition = 3750;
 
-  @Tunable("[Elevator] Max Elevator Deviation")
   public static double maxElevatorDeviation = 200;
 
-  @Tunable("[Elevator] Down Multiplier")
   public static double elevatorDownMult = 0.5;
 
-  @Tunable("[Elevator] Max Spike Time")
   public static double elevatorSpikeTime = 0;
 
-  @Tunable("[Elevator] Max Spike Current")
   public static double elevatorCurrentThreshold = 60;
 
   // WRIST
@@ -125,16 +105,12 @@ public class Tuning {
 
   public static int wristMaxAccel = 1000;
 
-  @Tunable("[Wrist] Peak Current Limit")
   public static int wristCurrentLimit = 30;
 
-  @Tunable("[Wrist] Peak Duration")
   public static int wristPeakDuration = 0;
 
-  @Tunable("[Wrist] Stall Current")
   public static double wristStallCurrent = 30;
 
-  @Tunable("[Wrist] Stop Tolerance")
   public static double wristTolerance = 50;
 
   public static double wristOutPosition = 8250;
@@ -147,7 +123,6 @@ public class Tuning {
 
   public static double wrist45FwdPosition = 6200;
 
-  @Tunable("[Wrist] Max Wrist Deviation")
   public static double maxWristDeviation = 200;
 
   // DRIVETRAIN
@@ -174,28 +149,27 @@ public class Tuning {
   public static double drivetrainEncoderTPU;
 
   // Units in inches and seconds
-  @Tunable("[MotionP] MaxVelocity")
+
   public static double maxVelocity = 40;
-  @Tunable("[MotionP] MaxVelocityFast")
+
   public static double maxVelocityFast = 80;
-  @Tunable("[MotionP] MaxAcceleration")
+
   public static double maxAcceleration = 40;
-  @Tunable("[MotionP] MaxJerk")
+
   public static double maxJerk = 2300;
-  @Tunable("[MotionP] SecondsFromNeutralToFull")
+
   public static double secondsFromNeutralToFull = 0;
-  @Tunable("[MotionP] SampleRate")
+
   public static int sampleRate = Config.SAMPLES_FAST;
-  @Tunable("[MotionP] TimeStep")
+
   public static double timeStep = 0.05;
   public static FitMethod fitMethod = FitMethod.HERMITE_CUBIC;
 
-  @Tunable("[MotionP] LeftEncoderTicksPerUnit")
   public static double lEncoderTicksPerUnit = 52;
-  @Tunable("[MotionP] RightEncoderTicksPerUnit")
+
   public static double rEncoderTicksPerUnit = 52;
-  @Tunable("[MotionP] WheelbaseWidth")
+
   public static double wheelbaseWidth = 25.091;
-  @Tunable("[MotionP] distanceBetweenWheels")
+
   public static double distanceBetweenWheels = 11.812;
 }
