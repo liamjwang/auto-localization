ed3df5e22a4983cae7b98653f29d0dd9d327cb34
Zachary Robinson <robinsonz@catlin.edu>
2018-03-14 15:04:18 -0700

    Create SpinnyTestRobot

---
+++ src/main/java/org/team1540/robot2018/SpinnyTestRobot.java
@@ -0,0 +1,101 @@
+package org.team1540.robot2018;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.team1540.base.util.SimpleCommand;
+import org.team1540.base.wrappers.ChickenTalon;
+
+/**
+ * Robot for testing the wheelbase width
+ */
+public class SpinnyTestRobot extends IterativeRobot {
+  private ChickenTalon driveLeftMotorA = new ChickenTalon(RobotMap.DRIVE_LEFT_A);
+  private ChickenTalon driveLeftMotorB = new ChickenTalon(RobotMap.DRIVE_LEFT_B);
+  private ChickenTalon driveLeftMotorC = new ChickenTalon(RobotMap.DRIVE_LEFT_C);
+  private ChickenTalon[] driveLeftMotors = new ChickenTalon[]{driveLeftMotorA, driveLeftMotorB, driveLeftMotorC};
+  private ChickenTalon driveRightMotorA = new ChickenTalon(RobotMap.DRIVE_RIGHT_A);
+  private ChickenTalon driveRightMotorB = new ChickenTalon(RobotMap.DRIVE_RIGHT_B);
+  private ChickenTalon driveRightMotorC = new ChickenTalon(RobotMap.DRIVE_RIGHT_C);
+  private ChickenTalon[] driveRightMotors = new ChickenTalon[]{driveRightMotorA, driveRightMotorB, driveRightMotorC};
+  private ChickenTalon[] driveMotorAll = new ChickenTalon[]{driveLeftMotorA, driveLeftMotorB, driveLeftMotorC, driveRightMotorA, driveRightMotorB, driveRightMotorC};
+  private ChickenTalon[] driveMotorMasters = new ChickenTalon[]{driveLeftMotorA, driveRightMotorA};
+
+  private Joystick joystick = new Joystick(0);
+
+  @Override
+  public void robotInit() {
+    driveLeftMotorA.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+    driveRightMotorA.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+
+    driveLeftMotorA.setSensorPhase(Tuning.isPandora);
+
+    for (ChickenTalon talon : driveLeftMotors) {
+      talon.setInverted(false);
+    }
+
+    driveRightMotorA.setSensorPhase(true);
+
+    for (ChickenTalon talon : driveRightMotors) {
+      talon.setInverted(true);
+    }
+
+    driveLeftMotorB.set(ControlMode.Follower, driveLeftMotorA.getDeviceID());
+    driveLeftMotorC.set(ControlMode.Follower, driveLeftMotorA.getDeviceID());
+
+    driveRightMotorB.set(ControlMode.Follower, driveRightMotorA.getDeviceID());
+    driveRightMotorC.set(ControlMode.Follower, driveRightMotorA.getDeviceID());
+
+    for (ChickenTalon talon : driveMotorAll) {
+      talon.setBrake(true);
+    }
+
+    for (ChickenTalon talon : driveMotorMasters) {
+      talon.config_kP(0, Tuning.drivetrainP);
+      talon.config_kI(0, Tuning.drivetrainI);
+      talon.config_kD(0, Tuning.drivetrainD);
+      talon.config_kF(0, Tuning.drivetrainF);
+      talon.config_IntegralZone(0, Tuning.drivetrainIZone);
+    }
+
+    for (ChickenTalon talon : driveMotorAll) {
+      talon.configClosedloopRamp(Tuning.drivetrainRampRate);
+      talon.configOpenloopRamp(Tuning.drivetrainRampRate);
+    }
+
+    // smartdash
+
+    Command cmd = new SimpleCommand("Zero DT", () -> {
+      driveLeftMotorA.setSelectedSensorPosition(0);
+      driveRightMotorA.setSelectedSensorPosition(0);
+    });
+    cmd.setRunWhenDisabled(true);
+    SmartDashboard.putData(cmd);
+  }
+
+  @Override
+  public void robotPeriodic() {
+    double leftDistance = driveLeftMotorA.getSelectedSensorPosition();
+    double rightDistance = -driveRightMotorA.getSelectedSensorPosition();
+
+    SmartDashboard.putNumber("Left Distance", leftDistance);
+    SmartDashboard.putNumber("Right Distance", rightDistance);
+
+    SmartDashboard.putNumber("Calculated width (assuming 10 rots)",
+        (((leftDistance + rightDistance) / 2) / (10 * Math.PI)) / Tuning.drivetrainEncoderTPU);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    if (joystick.getRawButton(1)) {
+      driveLeftMotorA.set(0.5);
+      driveRightMotorA.set(-0.5);
+    } else {
+      driveLeftMotorA.set(ControlMode.PercentOutput, 0);
+      driveRightMotorA.set(ControlMode.PercentOutput, 0);
+    }
+  }
+}
