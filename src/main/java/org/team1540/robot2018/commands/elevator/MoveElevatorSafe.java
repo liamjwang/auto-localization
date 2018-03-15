2a4bec36fb62758989c275164baafbb00534643c
Zachary Robinson <robinsonz@catlin.edu>
2018-03-15 08:55:44 -0700

    Replace anonymous ConditionalCommands with SimpleConditionalCommand

--- src/main/java/org/team1540/robot2018/commands/elevator/MoveElevatorSafe.java
+++ src/main/java/org/team1540/robot2018/commands/elevator/MoveElevatorSafe.java
@@ -1,7 +1,7 @@
 package org.team1540.robot2018.commands.elevator;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import edu.wpi.first.wpilibj.command.ConditionalCommand;
+import org.team1540.base.util.SimpleConditionalCommand;
 import org.team1540.robot2018.Robot;
 import org.team1540.robot2018.Tuning;
 import org.team1540.robot2018.commands.wrist.MoveWrist;
@@ -9,18 +9,15 @@ import org.team1540.robot2018.commands.wrist.MoveWrist;
 public class MoveElevatorSafe extends CommandGroup {
 
   public MoveElevatorSafe(boolean limitCurrent, double target) {
-    addSequential(new ConditionalCommand(new MoveWrist(Tuning.wristTransitPosition)) {
-      @Override
-      protected boolean condition() {
-        return ((target > Tuning.elevatorObstaclePosition
+    addSequential(new SimpleConditionalCommand(
+        () -> ((target > Tuning.elevatorObstaclePosition
             && target < Tuning.elevatorObstacleUpperPosition)
             || ((target > Tuning.elevatorObstacleUpperPosition
             && Robot.elevator.getPosition() < Tuning.elevatorObstacleUpperPosition)
             || (target < Tuning.elevatorObstaclePosition
             && Robot.elevator.getPosition() > Tuning.elevatorObstaclePosition)))
-            && Robot.wrist.getPosition() < Tuning.wristTransitPosition;
-      }
-    });
+            && Robot.wrist.getPosition() < Tuning.wristTransitPosition,
+        new MoveWrist(Tuning.wristTransitPosition)));
     addSequential(new MoveElevator(limitCurrent, target));
   }
 }
--- src/main/java/org/team1540/robot2018/commands/groups/GroundPosition.java
+++ src/main/java/org/team1540/robot2018/commands/groups/GroundPosition.java
@@ -1,8 +1,8 @@
 package org.team1540.robot2018.commands.groups;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import edu.wpi.first.wpilibj.command.ConditionalCommand;
 import edu.wpi.first.wpilibj.command.InstantCommand;
+import org.team1540.base.util.SimpleConditionalCommand;
 import org.team1540.robot2018.Robot;
 import org.team1540.robot2018.Tuning;
 import org.team1540.robot2018.commands.elevator.CalibrateElevator;
@@ -12,12 +12,9 @@ import org.team1540.robot2018.commands.wrist.CalibrateWrist;
 public class GroundPosition extends CommandGroup {
   public GroundPosition() {
     addSequential(new CalibrateWrist());
-    addSequential(new ConditionalCommand(new MoveElevator(true, Tuning.elevatorGroundPosition)) {
-      @Override
-      protected boolean condition() {
-        return Robot.elevator.getPosition() > Tuning.elevatorGroundPosition;
-      }
-    });
+    addSequential(new SimpleConditionalCommand(
+        () -> Robot.elevator.getPosition() > Tuning.elevatorGroundPosition,
+        new MoveElevator(true, Tuning.elevatorGroundPosition)));
     addSequential(new CalibrateElevator());
     addSequential(new InstantCommand());
   }
