4a352f6e8cc650560c7495ba1d8fcb16711f010d
wangl5 <wangl5@users.noreply.github.com>
2018-03-16 20:21:36 -0700

    Add debugging messages and fix gyro logic

--- src/main/java/org/team1540/robot2018/motion/FollowProfile.java
+++ src/main/java/org/team1540/robot2018/motion/FollowProfile.java
@@ -1,5 +1,10 @@
 package org.team1540.robot2018.motion;
 
+import static java.lang.Double.max;
+import static java.lang.Math.PI;
+import static java.lang.Math.abs;
+import static java.lang.StrictMath.min;
+
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
@@ -29,22 +34,39 @@ public class FollowProfile extends Command {
 
     double leftVelocity = (leftSegment.velocity * Tuning.drivetrainEncoderTPU) * 0.1;
 
+    double robotHeading = 2 * PI - Math.toRadians(
+        Robot.navx.getYaw() < 0 ? 360 + Robot.navx.getYaw() : Robot.navx.getYaw());
+    double desiredHeading = leftSegment.heading;
+
+    double regularError = robotHeading - desiredHeading;
+    double differentError =
+        (2 * PI - max(robotHeading, desiredHeading)) - min(robotHeading, desiredHeading);
+
+    double headingError = abs(regularError) < abs(differentError) ? regularError : differentError;
+
     // heading is negated for the left side only so that negative heading errors (i.e. too far right)
     // result in the left side slowing but the right side speeding up
     double leftVelocitySetpoint = leftVelocity + getVelocityModification(
         (leftSegment.acceleration * Tuning.drivetrainEncoderTPU) * 0.1,
+        leftVelocity - Robot.drivetrain.getLeftVelocity(),
         (leftSegment.position * Tuning.drivetrainEncoderTPU) - Robot.drivetrain.getLeftPosition(),
-        -(leftSegment.heading - Math.toRadians(Robot.gyro.getAngle())));
+        headingError);
 
     double rightVelocity = (rightSegment.velocity * Tuning.drivetrainEncoderTPU) * 0.1;
 
     double rightVelocitySetpoint = rightVelocity + getVelocityModification(
         (rightSegment.acceleration * Tuning.drivetrainEncoderTPU) * 0.1,
+        rightVelocity - Robot.drivetrain.getLeftVelocity(),
         (rightSegment.position * Tuning.drivetrainEncoderTPU) - Robot.drivetrain.getRightPosition(),
-        (rightSegment.heading - Math.toRadians(Robot.gyro.getAngle())));
+        -headingError);
 
     Robot.drivetrain.setLeftVelocity(leftVelocitySetpoint);
     Robot.drivetrain.setRightVelocity(rightVelocitySetpoint);
+    System.out.println("Gyro Angle " + robotHeading);
+    System.out.println("Intended Heading " + desiredHeading);
+
+    System.out.println("Error" + headingError);
+    // System.out.println("Heading Correction "+ (Tuning.profileHeadingP *(leftSegment.heading - Math.toRadians(-Robot.navx.getYaw()))));
   }
 
   @Override
@@ -102,15 +124,17 @@ public class FollowProfile extends Command {
     return (x - lowX) * (highY - lowY) / (highX - lowX) + lowY;
   }
 
-  private static double getVelocityModification(double trajAccelSetpoint, double positionError, double headingError) {
+  private static double getVelocityModification(double trajAccelSetpoint, double velocityError, double positionError, double headingError) {
     return Tuning.profileHeadingP * headingError +
         Tuning.profileAccelP * trajAccelSetpoint +
+        Tuning.profileVelocityP * velocityError +
         Tuning.profilePositionP * positionError;
   }
 
   @Override
   protected void initialize() {
     timer.reset();
+    Robot.navx.zeroYaw();
     timer.start();
     loop.startPeriodic(Tuning.profileLoopFrequency);
     finished = false;
@@ -119,5 +143,6 @@ public class FollowProfile extends Command {
   @Override
   protected void end() {
     loop.stop();
+    System.out.println("Profile Done!");
   }
 }
