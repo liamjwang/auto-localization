b46ac09ac9ceb1b2750ba834d3102ca68bab98a7
Zachary Robinson <robinsonz@catlin.edu>
2018-04-02 18:56:52 -0700

    Use new profile follower in SingleCubeSwitchAuto

--- src/main/java/org/team1540/robot2018/commands/auto/sequences/SingleCubeSwitchAuto.java
+++ src/main/java/org/team1540/robot2018/commands/auto/sequences/SingleCubeSwitchAuto.java
@@ -1,16 +1,19 @@
 package org.team1540.robot2018.commands.auto.sequences;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.team1540.robot2018.CSVProfileManager.DriveProfile;
+import org.team1540.robot2018.Robot;
 import org.team1540.robot2018.Tuning;
-import org.team1540.robot2018.commands.auto.RunProfile;
 import org.team1540.robot2018.commands.intake.Eject;
 import org.team1540.robot2018.commands.wrist.CalibrateWrist;
 import org.team1540.robot2018.commands.wrist.MoveWrist;
+import org.team1540.robot2018.motion.FollowProfile;
 
 public class SingleCubeSwitchAuto extends CommandGroup {
   public SingleCubeSwitchAuto(String profileName) {
     addParallel(new MoveWrist(Tuning.wrist45BackPosition));
-    addSequential(new RunProfile(profileName));
+    DriveProfile profile = Robot.profiles.getProfile(profileName);
+    addSequential(new FollowProfile(profile.getLeft(), profile.getRight()));
     addSequential(new Eject(1));
     addSequential(new CalibrateWrist());
   }
