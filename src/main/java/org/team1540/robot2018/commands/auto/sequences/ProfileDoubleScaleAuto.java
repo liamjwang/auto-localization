e09f735be6aafeac299484d381593cdc8a488c8c
Zachary Robinson <robinsonz@catlin.edu>
2018-04-04 22:03:29 -0700

    Create ProfileDoubleScaleAuto

---
+++ src/main/java/org/team1540/robot2018/commands/auto/sequences/ProfileDoubleScaleAuto.java
@@ -0,0 +1,41 @@
+package org.team1540.robot2018.commands.auto.sequences;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.team1540.robot2018.Tuning;
+import org.team1540.robot2018.commands.elevator.MoveElevator;
+import org.team1540.robot2018.commands.groups.GroundPosition;
+import org.team1540.robot2018.commands.intake.AutoIntake;
+import org.team1540.robot2018.commands.intake.Eject;
+import org.team1540.robot2018.commands.wrist.MoveWrist;
+import org.team1540.robot2018.motion.FollowProfile;
+
+public class ProfileDoubleScaleAuto extends CommandGroup {
+  public ProfileDoubleScaleAuto(String firstApproachProfile, String backupProfile, String secondApproachProfile) {
+    // drop the first cube
+    addSequential(new ProfileScaleAuto(firstApproachProfile));
+
+    // we end in ground position
+
+    // run the backup profile and pick up the cube
+    // this causes the command group to execute these commands at the same time but wait for both to finish before moving on
+    addSequential(new CommandGroup() {
+      {
+        addParallel(new FollowProfile(backupProfile));
+        addParallel(new AutoIntake());
+      }
+    });
+
+    // go back to the scale, raise elevator, eject cube
+    addSequential(new CommandGroup() {
+      {
+        addParallel(new FollowProfile(secondApproachProfile));
+        addParallel(new MoveWrist(Tuning.wristTransitPosition));
+        addSequential(new MoveElevator(false, Tuning.elevatorMaxPosition));
+        addSequential(new MoveWrist(Tuning.wristBackPosition));
+      }
+    });
+
+    addSequential(new Eject(Tuning.intakeEjectSpeedAuto));
+    addSequential(new GroundPosition());
+  }
+}
