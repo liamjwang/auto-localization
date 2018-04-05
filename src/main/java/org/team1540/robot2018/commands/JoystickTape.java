e9a2c1638239bd262dcba09bf84d35b547030228
wangl5 <wangl5@users.noreply.github.com>
2018-04-04 20:00:36 -0700

    Make tape measure retract as winch retracts

--- src/main/java/org/team1540/robot2018/commands/JoystickTape.java
+++ src/main/java/org/team1540/robot2018/commands/JoystickTape.java
@@ -3,7 +3,6 @@ package org.team1540.robot2018.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import org.team1540.robot2018.OI;
 import org.team1540.robot2018.Robot;
-import org.team1540.robot2018.Tuning;
 
 public class JoystickTape extends Command {
 
@@ -14,10 +13,10 @@ public class JoystickTape extends Command {
   @Override
   protected void execute() {
     if (OI.getTapeEnabled()) {
-      Robot.tape.set(OI.getTapeAxis());
+      Robot.tape.set(OI.getTapeAxis() - (OI.getWinchInAxis() * 1.5));
     }
     else {
-      Robot.tape.stop();
+      Robot.tape.set(-(OI.getWinchInAxis() * 1.5));
     }
   }
 
