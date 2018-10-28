320e09853755f6806973da8b37bf4b30921f043e
wangl5 <wangl5@users.noreply.github.com>
2018-10-27 17:34:01 -0700

    Update loc accum to use left and right

--- src/main/java/org/team1540/localization2D/LocalizationAccum2D.java
+++ src/main/java/org/team1540/localization2D/LocalizationAccum2D.java
@@ -6,7 +6,8 @@ public class LocalizationAccum2D {
   private double ypos;
   private double theta;
 
-  private double distancePrev;
+  private double distancePrevLeft;
+  private double distancePrevRight;
   private double angleRadsPrev;
 
   public LocalizationAccum2D() {
@@ -18,34 +19,46 @@ public class LocalizationAccum2D {
     ypos = 0;
     theta = 0;
 
-    distancePrev = 0;
+    distancePrevLeft = 0;
+    distancePrevRight = 0;
     angleRadsPrev = 0;
   }
 
   /**
-   * @param distance
+   * @param distanceLeft
+   * Absolute distance of wheels
+   * @param distanceRight
    * Absolute distance of wheels
    * @param angleRads
    * Absolute angle in radians
    */
-  public void update(double distance, double angleRads) {
-    double deltaDistance = distance - distancePrev;
+  public void update(double distanceLeft, double distanceRight, double angleRads) {
+    double deltaDistanceLeft = distanceLeft - distancePrevLeft;
+    double deltaDistanceRight = distanceRight - distancePrevRight;
     double deltaRads = angleRads - angleRadsPrev;
 
-    xpos += calcDeltaX(deltaDistance, deltaRads);
-    ypos += calcDeltaY(deltaDistance, deltaRads);
+    distancePrevLeft = deltaDistanceLeft;
+    distancePrevRight = deltaDistanceRight;
+    angleRadsPrev = deltaRads;
+
+    // Calculate radius of turn
+    double avgRadius = (calcRadius(deltaDistanceLeft, deltaRads) + calcRadius(deltaDistanceRight, deltaRads)) /2;
+
+    // Increment the accumulators
+    xpos += calcDeltaX(avgRadius, deltaRads);
+    ypos += calcDeltaY(avgRadius, deltaRads);
   }
 
   private double calcRadius(double arcLength, double angleRads) {
     return arcLength/angleRads;
   }
 
-  private double calcDeltaX(double deltaDistance, double deltaRads) {
-    return calcRadius(deltaDistance, deltaRads)*(1-Math.cos(deltaRads));
+  private double calcDeltaX(double radius, double deltaRads) {
+    return radius*(1-Math.cos(deltaRads));
   }
 
-  private double calcDeltaY(double deltaDistance, double deltaRads) {
-    return calcRadius(deltaDistance, deltaRads)*Math.sin(deltaRads);
+  private double calcDeltaY(double radius, double deltaRads) {
+    return radius*Math.sin(deltaRads);
   }
 
   public double getXpos() {
