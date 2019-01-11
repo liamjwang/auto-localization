ea8e7d68313f2c55a1fc97bb9ec2c99189a2c2be
liamjwang <liamjwang@users.github.noreply.com>
2019-01-10 18:14:14 -0800

    Add limelight localization class

---
+++ src/main/java/org/team1540/localization2D/LimelightLocalization.java
@@ -0,0 +1,18 @@
+package org.team1540.localization2D;
+
+import org.apache.commons.math3.geometry.Point;
+import org.apache.commons.math3.geometry.euclidean.threed.Line;
+
+import java.awt.geom.Point2D;
+
+public class LimelightLocalization {
+
+    public static Point2D anglesFromScreenSpace(Point2D point) {
+        //
+    }
+
+    public static Line lineFromScreenAngle(Point2D point) {
+
+    }
+
+}
