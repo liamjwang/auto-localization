ec0071a003223ee64b2839a4e000251c18326264
liamjwang <liamjwang@users.github.noreply.com>
2019-01-10 18:29:24 -0800

    Add more dummy methods

--- src/main/java/org/team1540/localization2D/LimelightLocalization.java
+++ src/main/java/org/team1540/localization2D/LimelightLocalization.java
@@ -2,17 +2,22 @@ package org.team1540.localization2D;
 
 import org.apache.commons.math3.geometry.Point;
 import org.apache.commons.math3.geometry.euclidean.threed.Line;
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 
 import java.awt.geom.Point2D;
 
 public class LimelightLocalization {
 
     public static Point2D anglesFromScreenSpace(Point2D point) {
-        //
+        //http://docs.limelightvision.io/en/latest/theory.html#from-pixels-to-angles
+        return null;
     }
 
-    public static Line lineFromScreenAngle(Point2D point) {
-
+    public static Vector3D lineFromScreenAngle(Point2D angles2D, Vector3D cameraPosition) {
+        // Angle from camera to
+        return null;
     }
+
+    public static
 
 }
