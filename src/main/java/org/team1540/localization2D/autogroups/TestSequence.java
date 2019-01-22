cfa3a5a9330f96b5c4905c424f2430c5f8576e30
Liam Wang <5054950+liamjwang@users.noreply.github.com>
2019-01-21 16:21:59 -0800

    Make TestSequence use a constructor input

--- src/main/java/org/team1540/localization2D/autogroups/TestSequence.java
+++ src/main/java/org/team1540/localization2D/autogroups/TestSequence.java
@@ -6,16 +6,14 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.team1540.localization2D.commands.drivetrain.UDPVelocityTwistDrive;
 
 public class TestSequence extends CommandGroup {
-    public TestSequence() {
+    public TestSequence(double pos_x, double pos_y, double ori_z) {
         // addSequential(new PointToTarget());
         // addSequential(new UDPVelocityTwistDrive(
         //         SmartDashboard.getNumber("vision_position_x", 0),
         //         SmartDashboard.getNumber("vision_position_y", 0),
         //         SmartDashboard.getNumber("vision_orientation_z", 0), false));
         addSequential(new UDPVelocityTwistDrive(
-                SmartDashboard.getNumber("limelight-pose/position/x", 0),
-                SmartDashboard.getNumber("limelight-pose/position/y", 0),
-                SmartDashboard.getNumber("limelight-pose/orientation/z", 0), false));
+                pos_x, pos_y, ori_z, false));
 //        addSequential(new UDPVelocityTwistDrive(2, -0.5, 0.8, false));
         addSequential(new TimedCommand(1));
 //        addSequential(new UDPVelocityTwistDrive(0.3, 0, 0, true));
