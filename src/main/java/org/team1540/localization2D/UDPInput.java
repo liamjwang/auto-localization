15b41785cc14cdd50c1fab6a75a3371e531782b7
Liam Wang <5054950+liamjwang@users.noreply.github.com>
2019-01-25 16:19:47 -0800

    Add UDPInput for drive pipeline

---
+++ src/main/java/org/team1540/localization2D/UDPInput.java
@@ -0,0 +1,25 @@
+package org.team1540.localization2D;
+
+import java.util.OptionalDouble;
+import org.team1540.rooster.drive.pipeline.DriveData;
+import org.team1540.rooster.drive.pipeline.Input;
+import org.team1540.rooster.drive.pipeline.TankDriveData;
+
+public class UDPInput implements Input<TankDriveData> {
+  private double cmdVelX = 0;
+  private double cmdVelOmega = 0;
+
+  public void setCmdVelX(double cmdVelX) {this.cmdVelX = cmdVelX;}
+  public void setCmdVelTheta(double cmdVelOmega) {this.cmdVelOmega = cmdVelOmega;}
+
+  @Override
+  public TankDriveData get() {
+    double leftSetpoint =
+        (cmdVelX - cmdVelOmega * Tuning.drivetrainRadius);
+            // * Tuning.drivetrainTicksPerMeter / 10;
+    double rightSetpoint =
+        (cmdVelX + cmdVelOmega * Tuning.drivetrainRadius);
+            // * Tuning.drivetrainTicksPerMeter / 10;
+    return new TankDriveData(new DriveData(OptionalDouble.of(leftSetpoint)), new DriveData(OptionalDouble.of(rightSetpoint)), OptionalDouble.empty(), OptionalDouble.empty());
+  }
+}
