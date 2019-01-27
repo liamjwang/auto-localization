4f6160b666ffa66a766f8951c0190c25b581fbd0
wangl <{ID}+{username}@users.noreply.github.com`>
2019-01-27 11:40:46 -0800

    Add doc to Pose2D class

---
+++ src/main/java/org/team1540/localization2D/datastructures/Pose2D.java
@@ -0,0 +1,23 @@
+package org.team1540.localization2D.datastructures;
+
+/**
+ *  2D pose data structure class
+ */
+public class Pose2D {
+  public static final Pose2D ZERO = new Pose2D(0, 0, 0);
+
+  public double x = 0;
+  public double y = 0;
+  public double theta = 0;
+
+  /**
+   * @param x Distance in meters in the X direction
+   * @param y Distance in meters in the Y direction
+   * @param theta Angle in radians between -PI and PI
+   */
+  public Pose2D(double x, double y, double theta) {
+    this.x = x;
+    this.y = y;
+    this.theta = theta;
+  }
+}
