cfb0010d75e50f56a32c357b79343cfe4d66645a
wangl <{ID}+{username}@users.noreply.github.com`>
2019-01-27 11:41:09 -0800

    Rename Transform to Transform3D

--- src/main/java/org/team1540/localization2D/datastructures/Transform3D.java
+++ src/main/java/org/team1540/localization2D/datastructures/Transform3D.java
@@ -3,13 +3,13 @@ package org.team1540.localization2D.datastructures;
 import org.apache.commons.math3.geometry.euclidean.threed.Rotation;
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 
-public class Transform {
-  public static final Transform ZERO = new Transform(Vector3D.ZERO, Rotation.IDENTITY);
+public class Transform3D {
+  public static final Transform3D ZERO = new Transform3D(Vector3D.ZERO, Rotation.IDENTITY);
 
   public Vector3D position;
   public Rotation orientation;
 
-  public Transform(Vector3D position, Rotation orientation) {
+  public Transform3D(Vector3D position, Rotation orientation) {
     this.position = position;
     this.orientation = orientation;
   }
