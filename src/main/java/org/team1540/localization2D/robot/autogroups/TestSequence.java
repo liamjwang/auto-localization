eb645de42a4e89d76b4471f939fe73e54641a1da
wangl <{ID}+{username}@users.noreply.github.com`>
2019-01-27 23:04:27 -0800

    Refactor UDP commands to use refactored classes

--- src/main/java/org/team1540/localization2D/robot/autogroups/TestSequence.java
+++ src/main/java/org/team1540/localization2D/robot/autogroups/TestSequence.java
@@ -2,24 +2,13 @@ package org.team1540.localization2D.robot.autogroups;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.TimedCommand;
+import org.team1540.localization2D.datastructures.twod.Transform2D;
 import org.team1540.localization2D.robot.commands.drivetrain.UDPVelocityTwistDrive;
 
 public class TestSequence extends CommandGroup {
-    public TestSequence(double pos_x, double pos_y, double ori_z) {
-        // addSequential(new PointToTarget());
-        // addSequential(new UDPVelocityTwistDrive(
-        //         SmartDashboard.getNumber("vision_position_x", 0),
-        //         SmartDashboard.getNumber("vision_position_y", 0),
-        //         SmartDashboard.getNumber("vision_orientation_z", 0), false));
-        addSequential(new UDPVelocityTwistDrive(
-                pos_x, pos_y, ori_z, false));
-//        addSequential(new UDPVelocityTwistDrive(2, -0.5, 0.8, false));
+  public TestSequence() {
+    addSequential(new UDPVelocityTwistDrive(new Transform2D(1, 0, 0), false));
         addSequential(new TimedCommand(1));
-//        addSequential(new UDPVelocityTwistDrive(0.3, 0, 0, true));
-//        addSequential(new UDPVelocityTwistDrive(2, 0.5, Math.PI/2, true));
-//        addSequential(new UDPVelocityTwistDrive(0.3, 0, 0, true));
-//        addSequential(new UDPVelocityTwistDrive(2, 0.5, -Math.PI/2, true));
-//        addSequential(new UDPVelocityTwistDrive(0.3, 0, 0, true));
-        addSequential(new UDPVelocityTwistDrive(0, 0, 0, true));
+    addSequential(new UDPVelocityTwistDrive(Transform2D.ZERO, true));
     }
 }
--- src/main/java/org/team1540/localization2D/robot/commands/drivetrain/UDPVelocityTwistDrive.java
+++ src/main/java/org/team1540/localization2D/robot/commands/drivetrain/UDPVelocityTwistDrive.java
@@ -1,97 +1,104 @@
 package org.team1540.localization2D.robot.commands.drivetrain;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.team1540.localization2D.datastructures.twod.Transform2D;
+import org.team1540.localization2D.datastructures.twod.Twist2D;
 import org.team1540.localization2D.robot.Robot;
+import org.team1540.localization2D.robot.TankDriveTwist2DInput;
 import org.team1540.localization2D.robot.Tuning;
+import org.team1540.localization2D.utils.TrigUtils;
+import org.team1540.rooster.drive.pipeline.CTREOutput;
+import org.team1540.rooster.drive.pipeline.FeedForwardProcessor;
+import org.team1540.rooster.drive.pipeline.UnitScaler;
+import org.team1540.rooster.util.Executable;
+import org.team1540.rooster.wrappers.RevBlinken.ColorPattern;
 
 public class UDPVelocityTwistDrive extends Command {
-    double xGoal = 0;
-    double yGoal = 0;
-    double angleGoal = 0;
-    boolean freeGoalVel = false;
+  Transform2D goal;
 
-    boolean checkEnd = false;
+  private Executable pipeline;
+  private TankDriveTwist2DInput twist2DInput;
 
-  public UDPVelocityTwistDrive(double xGoal, double yGoal, double angleGoal, boolean freeGoalVel) {
-      this.xGoal = xGoal;
-      this.yGoal = yGoal;
-      this.angleGoal = angleGoal;
-      this.freeGoalVel = freeGoalVel;
+  boolean freeGoalVel;
 
-      this.checkEnd = true;
-      requires(Robot.drivetrain);
+  boolean checkEnd;
+
+  public UDPVelocityTwistDrive(Transform2D goal, boolean freeGoalVel) {
+    this.goal = goal;
+    this.freeGoalVel = freeGoalVel;
+
+    this.checkEnd = true;
+    requires(Robot.drivetrain);
   }
-    public UDPVelocityTwistDrive() {
-      this.checkEnd = false;
-        requires(Robot.drivetrain);
-    }
 
-    @Override
-    protected void initialize() {
+  public UDPVelocityTwistDrive() {
+    this.checkEnd = false;
+    this.freeGoalVel = false;
+    requires(Robot.drivetrain);
+  }
+
+  @Override
+  protected void initialize() {
+    Robot.drivetrain.reset();
+    Robot.drivetrain.configTalonsForVelocity();
+
+    twist2DInput = new TankDriveTwist2DInput(Tuning.drivetrainRadius);
+    pipeline = twist2DInput
+        .then(new FeedForwardProcessor(0.27667, 0.054083, 0.08694))
+        .then(new UnitScaler(Tuning.drivetrainTicksPerMeter, 10))
+        .then(new CTREOutput(Robot.drivetrain.driveLeftMotorA, Robot.drivetrain.driveRightMotorA, true));
+
     if (checkEnd) {
-      System.out.println("Set goal to " + xGoal);
-      SmartDashboard.putNumber("--------goal_position_x", xGoal);
-      SmartDashboard.putNumber("--------goal_position_y", yGoal);
-      SmartDashboard.putNumber("--------goal_orientation_z", angleGoal);
-      SmartDashboard.putBoolean("--------free_goal_vel", freeGoalVel);
-      Robot.serv.setGoal(xGoal, yGoal, angleGoal);
-    }
-        Robot.drivetrain.reset();
-        Robot.drivetrain.configTalonsForVelocity();
-    }
+      Robot.leds.set(ColorPattern.CHASE_BLUE);
 
-    @Override
-  protected void execute() {
-    double cmdVelX = 0;
-    double cmdVelOmega = 0;
-    if (Robot.serv != null) {
-      cmdVelX = Robot.serv.getCmdVelX();
-      cmdVelOmega = Robot.serv.getCmdVelTheta();
+      NetworkTable tebConfigTable = NetworkTableInstance.getDefault().getTable("TEBPlanner/Config");
+      tebConfigTable.getEntry("TebReset").setBoolean(true);
+      tebConfigTable.getEntry("MaxVelX").setNumber(2.0);
+      tebConfigTable.getEntry("MaxVelXBackwards").setNumber(1.5);
+      tebConfigTable.getEntry("AccLimX").setNumber(0.7);
+      tebConfigTable.getEntry("MaxVelTheta").setNumber(5.0);
+      tebConfigTable.getEntry("AccLimTheta").setNumber(6.0);
+      updateGoal();
     }
-//      System.out.println("Cmd Vel " + cmdVelX + " Omega " + cmdVelOmega);
-      double leftSetpoint = (cmdVelX-cmdVelOmega*Tuning.drivetrainRadius)*Tuning.drivetrainTicksPerMeter/10;
-    double rightSetpoint = (cmdVelX+cmdVelOmega*Tuning.drivetrainRadius)*Tuning.drivetrainTicksPerMeter/10;
-    Robot.drivetrain.setLeftVelocity(leftSetpoint);
-    Robot.drivetrain.setRightVelocity(rightSetpoint);
-    SmartDashboard.putNumber("debug-setpoint-left", leftSetpoint);
-    SmartDashboard.putNumber("debug-setpoint-right", rightSetpoint);
-    SmartDashboard.putNumber("debug-velocity-left", Robot.drivetrain.getLeftVelocity());
-    SmartDashboard.putNumber("debug-velocity-right", Robot.drivetrain.getRightVelocity());
   }
 
-    @Override
-    protected boolean isFinished() {
-      if (!this.checkEnd) {
-          return false;
-      }
-        double xError = xGoal - Robot.getPosX();
-        double yError = yGoal - Robot.getPosY();
-        double angleError = angleGoal*180/Math.PI - (-Robot.navx.getYaw());
-        while (angleError > 180) {
-            angleError -= 360;
-        }
-        while (angleError < -180) {
-            angleError += 360;
-        }
-        System.out.println(
-                "xError "+xError+
-                        " yError "+yError+
-                        " angleError "+angleError);
-        boolean finished = Math.abs(xError) < 0.04 &&
-                Math.abs(yError) < 0.04 &&
-                Math.abs(angleError) < 1;
-        if (finished) {
-            System.out.println("Close to goal: " + xGoal + " " + yGoal);
-            Robot.drivetrain.setLeftVelocity(0);
-            Robot.drivetrain.setRightVelocity(0);
-        }
-        return finished;
-    }
+  private void updateGoal() {
+    double xGoal = SmartDashboard.getNumber("limelight-pose/position/x", 0);
+    double yGoal = SmartDashboard.getNumber("limelight-pose/position/y", 0);
+    double angleGoal = SmartDashboard.getNumber("limelight-pose/orientation/z", 0);
 
-    public static double AngleDifference( double angle1, double angle2 )
-    {
-        double diff = ( angle2 - angle1 + 180 ) % 360 - 180;
-        return diff < -180 ? diff + 360 : diff;
+    Robot.udpSender.setGoal(new Transform2D(xGoal, yGoal, angleGoal));
+  }
+
+  @Override
+  protected void execute() {
+    Twist2D cmdVel = Robot.udpReceiver.getCmdVel();
+    twist2DInput.setTwist(cmdVel);
+    pipeline.execute();
+  }
+
+  @Override
+  protected boolean isFinished() {
+    if (!this.checkEnd) {
+      return false;
     }
+    // isFinished Checking
+    Vector3D odomPosition = Robot.odometry.getOdomToBaseLink().getPosition(); // TODO: This should use javaTF
+    double xError = goal.getX() - odomPosition.getX();
+    double yError = goal.getY() - odomPosition.getY();
+    double angleError = TrigUtils.SignedAngleDifference(goal.getTheta(), Math.toRadians(-Robot.navx.getYaw())); // TODO: If this is the proper way to calculate signed angle, this should be moved to the TrigUtils class
+
+    boolean finished = Math.abs(xError) < 0.038 && // TODO: Make this a static function
+        Math.abs(yError) < 0.038 &&
+        Math.abs(angleError) < Math.toRadians(3);
+    if (finished) {
+      System.out.println("Close to goal: " + goal.getX() + " " + goal.getY());
+      Robot.drivetrain.stop();
+    }
+    return finished;
+  }
 }
