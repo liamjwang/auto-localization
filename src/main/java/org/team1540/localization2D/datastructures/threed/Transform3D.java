f6721203ea9c877d57d887c2a79340f66d5bc154
Liam Wang <5054950+liamjwang@users.noreply.github.com>
2019-01-29 08:53:16 -0800

    Add more methods to transform data structures

--- src/main/java/org/team1540/localization2D/datastructures/threed/Transform3D.java
+++ src/main/java/org/team1540/localization2D/datastructures/threed/Transform3D.java
@@ -4,6 +4,7 @@ import org.apache.commons.math3.geometry.euclidean.threed.Rotation;
 import org.apache.commons.math3.geometry.euclidean.threed.RotationConvention;
 import org.apache.commons.math3.geometry.euclidean.threed.RotationOrder;
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.team1540.localization2D.datastructures.twod.Transform2D;
 
 public class Transform3D {
   public static final Transform3D IDENTITY = new Transform3D(Vector3D.ZERO, Rotation.IDENTITY);
@@ -27,6 +28,10 @@ public class Transform3D {
   public Transform3D(double x, double y, double theta) {
     this.position = new Vector3D(x, y, 0);
     this.orientation = new Rotation(RotationOrder.XYZ, RotationConvention.FRAME_TRANSFORM, 0, 0, theta);
+  }
+
+  public Transform2D toTransform2D() {
+    return new Transform2D(this.position.getX(), this.position.getY(), this.orientation.getAngles(RotationOrder.XYZ, RotationConvention.FRAME_TRANSFORM)[2]);
   }
 
   public Vector3D getPosition() {
--- src/main/java/org/team1540/localization2D/datastructures/twod/Transform2D.java
+++ src/main/java/org/team1540/localization2D/datastructures/twod/Transform2D.java
@@ -1,6 +1,7 @@
 package org.team1540.localization2D.datastructures.twod;
 
 import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+import org.team1540.localization2D.datastructures.threed.Transform3D;
 
 /**
  * 2D pose data structure class
@@ -37,5 +38,23 @@ public class Transform2D {
 
   public Vector2D getPositionVector() {
     return new Vector2D(x, y);
+  }
+
+  public Transform3D toTransform3D() {
+    return new Transform3D(x, y, theta);
+  }
+
+  public Transform2D add(Transform2D other) {
+    Transform3D thisTransform3D = this.toTransform3D();
+    Transform3D otherTransform3D = other.toTransform3D();
+    Transform3D transform3D = new Transform3D(thisTransform3D.getOrientation().applyInverseTo(otherTransform3D.getPosition()).add(thisTransform3D.getPosition()), thisTransform3D.getOrientation().applyTo(otherTransform3D.getOrientation()));
+    return transform3D.toTransform2D();
+  }
+
+  public Transform2D subtract(Transform2D other) {
+    Transform3D thisTransform3D = this.toTransform3D();
+    Transform3D otherTransform3D = other.toTransform3D();
+    Transform3D transform3D = new Transform3D(thisTransform3D.getPosition().subtract(thisTransform3D.getOrientation().applyInverseTo(otherTransform3D.getPosition())), thisTransform3D.getOrientation().applyInverseTo(otherTransform3D.getOrientation()));
+    return transform3D.toTransform2D();
   }
 }
