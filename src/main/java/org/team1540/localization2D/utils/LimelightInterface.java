9878e3dcf2a4bd468454d625f9d6bc5d4c7382f6
Liam Wang <5054950+liamjwang@users.noreply.github.com>
2019-01-29 15:58:56 -0800

    Add LimelightInterface class

--- src/main/java/org/team1540/localization2D/utils/LimelightInterface.java
+++ src/main/java/org/team1540/localization2D/utils/LimelightInterface.java
@@ -1,4 +1,20 @@
 package org.team1540.localization2D.utils;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+
 public class LimelightInterface {
+  private final NetworkTable limetable;
+
+  public LimelightInterface(String name) {
+    limetable = NetworkTableInstance.getDefault().getTable(name);
+  }
+
+  public Vector2D getRawPoint(int id) {
+    return new Vector2D(
+        limetable.getEntry("tx" + id).getDouble(0),
+        limetable.getEntry("ty" + id).getDouble(0)
+    );
+  }
 }
