f258128fc53317a203287b534e9d1c5260ed7081
Liam Wang <5054950+liamjwang@users.noreply.github.com>
2019-01-29 16:26:42 -0800

    Update UDPAutoLineup

--- src/main/java/org/team1540/localization2D/robot/commands/drivetrain/UDPAutoLineup.java
+++ src/main/java/org/team1540/localization2D/robot/commands/drivetrain/UDPAutoLineup.java
@@ -3,12 +3,10 @@ package org.team1540.localization2D.robot.commands.drivetrain;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.apache.commons.math3.geometry.euclidean.threed.Rotation;
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
 import org.team1540.localization2D.datastructures.threed.Transform3D;
-import org.team1540.localization2D.datastructures.twod.Transform2D;
 import org.team1540.localization2D.datastructures.twod.Twist2D;
 import org.team1540.localization2D.robot.Robot;
 import org.team1540.localization2D.robot.TankDriveTwist2DInput;
@@ -22,13 +20,8 @@ import org.team1540.rooster.wrappers.RevBlinken.ColorPattern;
 
 public class UDPAutoLineup extends Command {
   Transform3D goal;
-
   private Executable pipeline;
   private TankDriveTwist2DInput twist2DInput;
-
-  long finishedTime = 0;
-  private long lastUpdate = 0;
-
 
   public UDPAutoLineup() {
     requires(Robot.drivetrain);
@@ -44,8 +37,6 @@ public class UDPAutoLineup extends Command {
   protected void initialize() {
     Robot.drivetrain.reset();
     Robot.drivetrain.configTalonsForVelocity();
-
-    lastUpdate = System.currentTimeMillis();
 
     Robot.leds.set(ColorPattern.CHASE_BLUE);
 
@@ -63,49 +54,38 @@ public class UDPAutoLineup extends Command {
     goal = Robot.limelightLocalization.getBaseLinkToVisionTarget()
         .add(new Transform3D(new Vector3D(-0.65, 0, 0), Rotation.IDENTITY));
     Robot.udpSender.setGoal(goal.toTransform2D());
+
     Transform3D via_point = goal.add(new Transform3D(-0.7, 0, 0));
     Robot.udpSender.setViaPoint(via_point.toTransform2D().getPositionVector());
   }
 
   @Override
   protected void execute() {
-    Vector3D odomPosition = Robot.wheelOdometry.getOdomToBaseLink().getPosition(); // TODO: This should use javaTF
-    Transform2D goal2D = goal.toTransform2D();
-    double angleError = TrigUtils.SignedAngleDifference(goal2D.getTheta(), Math.toRadians(-Robot.navx.getYaw())); // TODO: If this is the proper way to calculate signed angle, this should be moved to the TrigUtils class
-
-    double distanceError = goal2D.getPositionVector().distance(new Vector2D(odomPosition.getX(), odomPosition.getY()));
-
-    if (SmartDashboard.getBoolean("limelight-pose/correct", false) // TODO: Use Pose2D averaging
-        && (distanceError > 0.07) // TODO: Make this distance tunable
-        && finishedTime == 0
-    ) {
-      lastUpdate = System.currentTimeMillis();
+    if (Robot.limelightLocalization.attemptUpdatePose() && (getDistanceError() > 0.07)) { // TODO: Make this distance tunable
       updateGoal();
     }
 
-    // Send velocity commaand
+    // Send velocity command
     Twist2D cmdVel = Robot.udpReceiver.getCmdVel();
     twist2DInput.setTwist(cmdVel);
     pipeline.execute();
-
-    // isFinished Checking
-    double xError = goal2D.getX() - odomPosition.getX();
-    double yError = goal2D.getY() - odomPosition.getY();
-
-    boolean finished = Math.abs(xError) < 0.018 && // TODO: Make this a static function
-        Math.abs(yError) < 0.018 &&
-        Math.abs(angleError) < Math.toRadians(3);
-    if (finished && finishedTime == 0) {
-      this.finishedTime = System.currentTimeMillis();
-    }
   }
 
   @Override
   protected boolean isFinished() {
-    if (finishedTime != 0 && System.currentTimeMillis() > finishedTime + 0) { // TODO: Make the finished time tunable
+    if (getDistanceError() < 0.018 && Math.abs(getAngleError()) < Math.toRadians(3)) {
       Robot.drivetrain.stop();
       return true;
     }
     return false;
+  }
+
+  private double getDistanceError() {
+    Vector3D odomPosition = Robot.wheelOdometry.getOdomToBaseLink().getPosition(); // TODO: This should use javaTF
+    return goal.toTransform2D().getPositionVector().distance(new Vector2D(odomPosition.getX(), odomPosition.getY()));
+  }
+
+  private double getAngleError() {
+    return TrigUtils.SignedAngleDifference(goal.toTransform2D().getTheta(), Math.toRadians(-Robot.navx.getYaw()));
   }
 }
