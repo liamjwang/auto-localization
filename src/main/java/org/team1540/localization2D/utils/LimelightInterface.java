5a2c4b253f9ee417b71d503e9737db1725ad0eac
Liam Wang <5054950+liamjwang@users.noreply.github.com>
2019-01-29 16:06:36 -0800

    Refactor

--- src/main/java/org/team1540/localization2D/utils/LimelightInterface.java
+++ src/main/java/org/team1540/localization2D/utils/LimelightInterface.java
@@ -5,16 +5,16 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
 
 public class LimelightInterface {
-  private final NetworkTable limetable;
+  private final NetworkTable limelightTable;
 
   public LimelightInterface(String name) {
-    limetable = NetworkTableInstance.getDefault().getTable(name);
+    limelightTable = NetworkTableInstance.getDefault().getTable(name);
   }
 
   public Vector2D getRawPoint(int id) {
     return new Vector2D(
-        limetable.getEntry("tx" + id).getDouble(0),
-        limetable.getEntry("ty" + id).getDouble(0)
+        limelightTable.getEntry("tx" + id).getDouble(0),
+        limelightTable.getEntry("ty" + id).getDouble(0)
     );
   }
 }
